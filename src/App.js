6291bb3a88efe47c534e4b7034954025c4f91d53
newtesting01012000 <newtesting01012000@gmail.com>
2023-08-02 14:06:35 +1000

    testing pr agent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const newMapArrayWeird = [123, 234, 5555];
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {newMapArrayWeird.map((item) => (
+          <div>{item}</div>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
